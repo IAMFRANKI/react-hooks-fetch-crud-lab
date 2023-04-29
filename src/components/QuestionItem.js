4c1eadaed220ac037ec5a5cca5da76a83ffe89e6
frank <thmsfrank04@gmail.com>
2023-04-28 23:57:24 -0700

    Complete assignment

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 function QuestionItem({ question }) {
+  function QuestionItem({ question, onDeleteQuestion }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -8,6 +9,13 @@ function QuestionItem({ question }) {
       {answer}
     </option>
   ));
+  }
+
+  function handleDelete() {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    }).then(() => onDeleteQuestion(id));
+  }
 
   return (
     <li>
@@ -16,8 +24,19 @@ function QuestionItem({ question }) {
       <label>
         Correct Answer:
         <select defaultValue={correctIndex}>{options}</select>
+        <select
+          defaultValue={correctIndex}
+        >
+          {options}
+        </select>
+        <select defaultValue={correctIndex}>{options}</select>
       </label>
       <button>Delete Question</button>
+      <button
+      onClick={handleDelete}
+      >
+        Delete Question
+      </button>
     </li>
   );
 }
